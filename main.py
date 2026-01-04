aab52ecc95e8241b3bcd3192b74f1cc82ee2bb76
simplyskyler <intovivid@gmail.com>
2026-01-04 03:03:54 -0500

    G: seventh commit. Updated update/draw methods.

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def main():
 	clock = pygame.time.Clock()
 	dt = 0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	Player.containers = (updatable, drawable)
 	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 	asteroid = "too many"
 	while asteroid == "too many":
@@ -16,10 +19,12 @@ def main():
     			if event.type == pygame.QUIT:
         			return
 		screen.fill("black")
-		player.update(dt)
-		player.draw(screen)
+		for obj in updatable:
+			obj.update(dt)
+		for drw in drawable:
+			drw.draw(screen)
 		pygame.display.flip()
-		dt += clock.tick(60)/1000
+		dt = clock.tick(60)/1000
 	print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
